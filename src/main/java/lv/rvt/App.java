0b60807c603ca5d9693a086eaabf2da33084eab7
23DP4ESire <166484726+23DP4ESire@users.noreply.github.com>
2024-10-11 11:14:32 +0000

    Reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,31 +3,26 @@ package lv.rvt;
 import java.util.*;
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-      
-            System.out.println("Ievadi skaitli:");
-               int number = Integer.valueOf(scanner.nextLine());
-       
-            if (number < 0) {
-                System.out.println("Faktoriāls nav definēts negatīviem skaitļiem.");
-            } else {
-                long factorial = 1;
-                for (int i = 1; i <= number; i++) {
-                    factorial *= i;
-                }
-                System.out.println("Faktoriāls: " + factorial);
-            }
-       
-               scanner.close();  
-       
-       
+     
 
-    
+        System.out.print("How many times? ");
+        int count = scanner.nextInt();
 
+        int i = 0;
+        while (i < count) {
+            printText();
+            i++;
+        }
+                
+        scanner.close();
+        
+    }
 
-
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
     }
   
 }
