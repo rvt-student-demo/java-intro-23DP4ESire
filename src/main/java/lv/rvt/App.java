b811ecbe0e741fb5f72cc370ddf7fdb42e50351a
23DP4ESire <166484726+23DP4ESire@users.noreply.github.com>
2024-10-31 13:13:49 +0000

    Two Arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,14 +4,16 @@ import java.util.*;
 public class App 
 {
     public static void main(String[] args) {
-    int[] val = {0, 1, 2, 3}; 
-
-    int sum = 0;
-
-    for (int number : val) {
-        sum += number;
-    }
-    System.out.println( "Sum of all numbers = " + sum);
+        int[] val = {13, -4, 82, 17}; 
+        int[] twice = new int[val.length];
+        
+        System.out.println( "Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+       
+        for (int i = 0; i < val.length; i++) {
+            twice[i] = val[i] * 2;
+        }
+    
+        System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
 
     }
 }
