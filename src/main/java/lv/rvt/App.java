3f1adc0ab9bdd9047fd2372475c863956dcda8bf
23DP4ESire <166484726+23DP4ESire@users.noreply.github.com>
2024-10-31 13:24:55 +0000

    Same Sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,16 +5,14 @@ public class App
 {
     public static void main(String[] args) {
         int[] valA   = { 13, -22,  82,  17}; 
-        int[] valB   = {-12,  24, -79, -13};
-        int[] sum    = {  0,   0,   0,   0};
-        
-
-        sum[0] = valA[0] + valB[0];
-        sum[1] = valA[1] + valB[1];
-        sum[2] = valA[2] + valB[2];
-        sum[3] = valA[3] + valB[3];
+        int[] valB   = {  12,   47,   -57,   8};
+         
      
-        System.out.println( "sum: " + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+        System.out.println( "valA: " + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
+     
+        System.out.println( "valB: " + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
+    
+        System.out.println( "sum:  " + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
 
     }
 }
