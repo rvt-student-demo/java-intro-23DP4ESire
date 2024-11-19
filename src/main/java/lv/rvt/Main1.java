a5e527f98c16d0c1e0d3259582ae1b72d1424e78
23DP4ESire <166484726+23DP4ESire@users.noreply.github.com>
2024-11-19 13:34:54 +0000

    Payment Card (6 parts)

---
+++ src/main/java/lv/rvt/Main1.java
@@ -0,0 +1,27 @@
+package lv.rvt;
+
+public class Main1 {
+    public static void main1(String[] args) {
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+
+        paulsCard.eatHeartily();
+        System.out.println("Paul: " + paulsCard);
+
+        mattsCard.eatAffordably();
+        System.out.println("Matt: " + mattsCard);
+
+        paulsCard.addMoney(20);
+        System.out.println("Paul: " + paulsCard);
+
+        mattsCard.eatHeartily();
+        System.out.println("Matt: " + mattsCard);
+
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        System.out.println("Paul: " + paulsCard);
+
+        mattsCard.addMoney(50);
+        System.out.println("Matt: " + mattsCard);
+    }
+}
