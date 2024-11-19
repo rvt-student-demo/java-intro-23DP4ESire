13f878c08da036a1e6009306af5b2c0ae9e43e6f
23DP4ESire <166484726+23DP4ESire@users.noreply.github.com>
2024-11-19 13:27:07 +0000

    Statistics (4 parts)

---
+++ src/main/java/lv/rvt/Main.java
@@ -0,0 +1,36 @@
+package lv.rvt;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        Statistics allNumbers = new Statistics();
+        Statistics evenNumbers = new Statistics();
+        Statistics oddNumbers = new Statistics();
+
+        System.out.println("Enter numbers:");
+        
+        while (true) {
+            int number = scanner.nextInt();
+            
+            if (number == -1) {
+                break;
+            }
+            
+            allNumbers.addNumber(number);
+
+            if (number % 2 == 0) {
+                evenNumbers.addNumber(number);
+            }
+            else {
+                oddNumbers.addNumber(number);
+                scanner.close();
+            }
+        }
+
+        System.out.println("Sum: " + allNumbers.sum());
+        System.out.println("Sum of even numbers: " + evenNumbers.sum());
+        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
+    }
+}
