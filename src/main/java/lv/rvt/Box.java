8c418ca2c376a26cd04a2882f700e61df2c93061
Emīls Šīre <166484726+23DP4ESire@users.noreply.github.com>
2025-01-17 11:04:48 +0000

    Nesting Boxes

--- src/main/java/lv/rvt/Box.java
+++ src/main/java/lv/rvt/Box.java
@@ -15,12 +15,6 @@ public class Box {
       this(side, side, side);
   }
 
-  public Box(Box oldBox) {
-      this.width = oldBox.width;
-      this.height = oldBox.height;
-      this.length = oldBox.length;
-  }
-
   public double volume() {
       return width * height * length;
   }
@@ -29,12 +23,8 @@ public class Box {
       return 2 * (width * height + width * length + height * length);
   }
 
-  public Box biggerBox() {
-      return new Box(1.25 * width, 1.25 * height, 1.25 * length);
-  }
-
-  public Box smallerBox() {
-      return new Box(0.75 * width, 0.75 * height, 0.75 * length);
+  public boolean nests(Box outsideBox) {
+      return this.width <= outsideBox.width && this.height <= outsideBox.height && this.length <= outsideBox.length;
   }
 
   public double getWidth() {
